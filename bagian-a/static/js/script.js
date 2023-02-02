7c3beefd3bac1ed57c9be3a85fb494f30c8d333d
putriliza <putriliza05@gmail.com>
2023-02-02 21:20:57 +0700

    update output display, button, radio logic

--- bagian-a/static/js/script.js
+++ bagian-a/static/js/script.js
@@ -1,11 +1,11 @@
 const operation = document.getElementsByName('operation');
 const input_method = document.getElementsByName('input_method');
+const msg_file = document.getElementById('msg-file');
+const msg_text = document.getElementById('msg-text');
 
 // chage msg type based on input method
 input_method.forEach((im) => {
     im.addEventListener('change', () => {
-        const msg_file = document.getElementById('msg-file');
-        const msg_text = document.getElementById('msg-text');
         if (im.value === 'file') {
             msg_file.style.display = 'block';
             msg_text.style.display = 'none';
@@ -34,3 +34,33 @@ operation.forEach((op) => {
 //     }
 //     reader.readAsText(file);
 // });
+
+document.getElementsByName("show").forEach((item) => {
+    item.addEventListener("change", (e) => {
+        if (e.target.value == "without-space") {
+            document.getElementById("without-space-result").style.display = "block";
+            document.getElementById("group-of-5-result").style.display = "none";
+        } else {
+            document.getElementById("without-space-result").style.display = "none";
+            document.getElementById("group-of-5-result").style.display = "block";
+        }
+    });
+});
+
+// buat vigenere
+const type = document.getElementsByName("type");
+type.forEach((item) => {
+    item.addEventListener("change", (e) => {
+        if (e.target.value == "standard" || e.target.value == "autokey") {
+            document.getElementById("input-file-container").style.display = "block";
+            document.getElementById("input-manual-container").style.display = "block";
+        } else {
+            document.getElementById("input-file-container").style.display = "block";
+            document.getElementById("input-manual-container").style.display = "none";
+            document.getElementById("file").checked = true;
+            document.getElementById("manual").checked = false;
+            msg_file.style.display = 'block';
+            msg_text.style.display = 'none';
+        }
+    });
+});
